afa5e2cee7e6106f6d817df078aa3277c139d797
David Hernandez Vilalta <david.hernandez.vilalta@est.fib.upc.edu>
2017-06-27 15:48:14 +0200

    Initial commit

---
+++ app/src/main/java/com/davidhernandezvilaltagmail/pistoli/MainActivity.java
@@ -0,0 +1,30 @@
+package com.davidhernandezvilaltagmail.pistoli;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private int numero;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+        numero = 0;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart");
+        numero = 1;
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop");
+        numero = 2;
+    }
+}
